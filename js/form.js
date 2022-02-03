214426309009ccfd71e47b662d960447652db529
Blagoja95 <blagojevic.info@gmail.com>
2022-02-03 15:11:02 +0100

    html/css done

---
+++ js/form.js
@@ -0,0 +1,6 @@
+const email__form = document.querySelector('.form-email');
+const email__input = document.querySelector('.input-email');
+
+email__form.addEventListener('submit', function(){
+    if()
+})
